17fa947df8b6e4376fc42e542334b4c4d6dd68bf
zxhd <zxhd863943427@outlook.com>
2023-07-15 21:08:01 +0800

    reSolveWorkflow

--- src/workflowApi.ts
+++ src/workflowApi.ts
@@ -1,10 +1,11 @@
 import { sql, getBlockAttrs,setBlockAttrs } from "./api";
 import { Lute } from "siyuan";
-import { getProtyleCurrentElement,turnIntoTask } from "./workflow/utils";
+import { getProtyleCurrentElement,turnIntoTask,getProtyleElementById } from "./workflow/utils";
 export let workflowApi = { 
     sql:sql, 
     getBlockAttrs:getBlockAttrs,
     setBlockAttrs:setBlockAttrs,
     newNodeID:window.Lute.NewNodeID,
     getProtyleCurrentElement:getProtyleCurrentElement,
-    turnIntoTask:turnIntoTask }
+    turnIntoTask:turnIntoTask,
+    getProtyleElementById: getProtyleElementById }
